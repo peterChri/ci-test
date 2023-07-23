8df3769b4dc67890a16cdbb19a0851bded2d2f49
Peter Christiani <95409378+peterChri@users.noreply.github.com>
2023-07-23 14:25:33 +0200

    Create pep8-violation.py

---
+++ pep8-violation.py
@@ -0,0 +1,13 @@
+# https://peps.python.org/pep-0008/
+
+# Wrong:
+
+# Arguments on first line forbidden when not using vertical alignment.
+foo = long_function_name(var_one, var_two,
+    var_three, var_four)
+
+# Further indentation required as indentation is not distinguishable.
+def long_function_name(
+    var_one, var_two, var_three,
+    var_four):
+    print(var_one)
